d2ff883b140c146a23c0d35b850d77d2fa9a6b34
Nathan Winspear <n.winspear1@gmail.com>
2020-09-14 21:03:00 +1200

    updated routes

--- home/routes.py
+++ home/routes.py
@@ -1,8 +1,14 @@
 from flask import Blueprint, make_response
+from adapters.memoryrepository import MemoryRepository
 
 bp = Blueprint('home_blueprint', __name__)
+
+repo = MemoryRepository()
 
 
 @bp.route('/', methods=['GET'])
 def index():
-    return None
+    movies = repo.get_all_movies()
+    reviews = repo.get_all_reviews()
+
+    return make_response()
