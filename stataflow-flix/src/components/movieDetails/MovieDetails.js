bb38d234007389db07620017780f03d98057a67f
n-winspear <50559711+n-winspear@users.noreply.github.com>
2020-09-25 14:38:21 +1200

    updated loading

--- stataflow-flix/src/components/movieDetails/MovieDetails.js
+++ stataflow-flix/src/components/movieDetails/MovieDetails.js
@@ -1,13 +1,44 @@
 import React, { useState, useEffect } from "react";
+import { Box } from '@material-ui/core';
+import { makeStyles } from "@material-ui/core/styles";
+import CircularProgress from "@material-ui/core/CircularProgress";
+
+const useStyles = makeStyles(() => ({
+  loading: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexGrow: 1,
+    height: "calc(100vh - 64px)",
+  },
+  circularProgress: {
+    color: "#FDA74A",
+  },
+}));
 
 export default function MovieDetails(props) {
   const { match } = props;
+  const classes = useStyles();
+  const [isLoading, setLoading] = useState(true);
 
-  console.log(match);
+  useEffect(() => {
+    function loadData() {
+      setTimeout(() => {
+        setLoading(false)
+      },1000);
+    }
+    loadData();
+  });
 
-  return (
+  return isLoading ? (
+    <Box className={classes.loading}>
+      <CircularProgress className={classes.circularProgress} />
+    </Box>
+  ) : (
     <div>
       <h1>{match.params.movieID}</h1>
     </div>
+    
   );
+
 }
