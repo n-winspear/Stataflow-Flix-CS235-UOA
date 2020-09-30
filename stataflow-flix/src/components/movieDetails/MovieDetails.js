b1dc923fd8fe229774cad4ab7c26969e40775b6f
Nathan Winspear <n.winspear1@gmail.com>
2020-10-01 11:48:31 +1300

    Update MovieDetails.js

--- stataflow-flix/src/components/movieDetails/MovieDetails.js
+++ stataflow-flix/src/components/movieDetails/MovieDetails.js
@@ -47,123 +47,122 @@ export default function MovieDetails(props) {
       <CircularProgress className={classes.circularProgress} />
     </Box>
   ) : (
-    <Box className={classes.pageContent}>
-      <Grid container justify="center" alignItems="center">
-        <Grid
-          container
-          direction="column"
-          justify="center"
-          alignItems="center"
-          item
-          xs={12}
-          sm={6}
-          md={4}
-          lg={3}
-          xl={3}
-        >
-          <Grid item xs={false} sm={12}>
-            <Box className={classes.movieCoverPoster}>
-              <img
-                src={MovieCoverPoster}
-                alt={"Movie Cover"}
-                height={"100%"}
-                width={"auto"}
-              />
-            </Box>
-          </Grid>
-          <Grid item xs={12}>
-            <Box mt={2} ml={-20}>
-              <Typography>Genres:</Typography>
-              <Typography gutterBottom variant="h5">
-                {movieData.genres.map((genre, index) => {
-                  let genreText =
-                    index === movieData.genres.length - 1
-                      ? genre
-                      : `${genre}, `;
-                  return genreText;
-                })}
-              </Typography>
-            </Box>
-          </Grid>
-        </Grid>
-        <Grid
-          container
-          direction="column"
-          justify="center"
-          alignItems="flex-start"
-          item
-          xs={12}
-          sm={6}
-          md={8}
-          lg={9}
-          xl={9}
-        >
-          <Grid item xs={12}>
-            <Typography>Movie Title</Typography>
-          </Grid>
-          <Grid container direction="row" item xs={12}>
-            <Grid item xs={1}>
-              <Typography>Release</Typography>
+      <Box className={classes.pageContent}>
+        <Grid container justify="center" alignItems="center">
+          <Grid
+            container
+            direction="column"
+            justify="center"
+            alignItems="center"
+            item
+            xs={12}
+            sm={6}
+            md={4}
+            lg={3}
+            xl={3}
+          >
+            <Grid item xs={false} sm={12}>
+              <Box className={classes.movieCoverPoster}>
+                <img
+                  src={MovieCoverPoster}
+                  alt={"Movie Cover"}
+                  height={"100%"}
+                  width={"auto"}
+                />
+              </Box>
             </Grid>
-            <Grid item xs={1}>
-              <Typography>Runtime</Typography>
+            <Grid item xs={12}>
+              <Box mt={2} ml={-20}>
+                <Typography>Genres:</Typography>
+                <Typography gutterBottom variant="h5">
+                  {movieData.genres.map((genre, index) => {
+                    let genreText =
+                      index === movieData.genres.length - 1
+                        ? genre
+                        : `${genre}, `;
+                    return genreText;
+                  })}
+                </Typography>
+              </Box>
             </Grid>
-            <Grid item xs={1}>
-              <Typography>Rating</Typography>
-            </Grid>
-          </Grid>
-          <Grid item xs={12}>
-            <Typography>Description</Typography>
           </Grid>
           <Grid
             container
-            direction="row"
-            justify="flex-start"
-            alignItems="center"
+            direction="column"
+            justify="center"
+            alignItems="flex-start"
+            item
+            xs={12}
+            sm={6}
+            md={8}
+            lg={9}
+            xl={9}
           >
-            <Grid
-              container
-              direction="column"
-              item
-              xs={12}
-              sm={6}
-              md={4}
-              lg={3}
-              xl={2}
-            >
-              <Grid item xs={12}>
-                <Typography>Cast Member</Typography>
+            <Grid item xs={12}>
+              <Typography>{movieData.title}</Typography>
+            </Grid>
+            <Grid container direction="row" item xs={12}>
+              <Grid item xs={1}>
+                <Typography>{movieData.releaseYear}</Typography>
               </Grid>
-              <Grid item xs={12}>
-                <Typography>Cast Member</Typography>
+              <Grid item xs={1}>
+                <Typography>{movieData.runtime}</Typography>
               </Grid>
-              <Grid item xs={12}>
-                <Typography>Cast Member</Typography>
+              <Grid item xs={1}>
+                <Typography>{movieData.rating}</Typography>
               </Grid>
+            </Grid>
+            <Grid item xs={12}>
+              <Typography>{movieData.description}</Typography>
             </Grid>
             <Grid
               container
-              direction="column"
-              item
-              xs={12}
-              sm={6}
-              md={8}
-              lg={9}
-              xl={9}
+              direction="row"
+              justify="flex-start"
+              alignItems="center"
             >
-              <Grid item xs={12}>
-                <Typography>Review</Typography>
+              <Grid
+                container
+                direction="column"
+                item
+                xs={12}
+                sm={6}
+                md={4}
+                lg={3}
+                xl={2}
+              >
+                {
+                  movieData.actors.map((actor, index) => {
+                    <Grid item xs={12}>
+                      <Typography key={index}>{actor.fullName}</Typography>
+                    </Grid>
+
+                  })
+                }
               </Grid>
-              <Grid item xs={12}>
-                <Typography>Review</Typography>
-              </Grid>
-              <Grid item xs={12}>
-                <Typography>Review</Typography>
+              <Grid
+                container
+                direction="column"
+                item
+                xs={12}
+                sm={6}
+                md={8}
+                lg={9}
+                xl={9}
+              >
+                <Grid item xs={12}>
+                  <Typography>Review</Typography>
+                </Grid>
+                <Grid item xs={12}>
+                  <Typography>Review</Typography>
+                </Grid>
+                <Grid item xs={12}>
+                  <Typography>Review</Typography>
+                </Grid>
               </Grid>
             </Grid>
           </Grid>
         </Grid>
-      </Grid>
-    </Box>
-  );
+      </Box>
+    );
 }
